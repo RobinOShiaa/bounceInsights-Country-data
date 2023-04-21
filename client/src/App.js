bd3b455410c9f7d109881942c598709a37cd040f
RobinOShiaa <oshea.robinho@gmail.com>
2023-04-21 18:50:55 +0100

    fix: width of notification

--- client/src/App.js
+++ client/src/App.js
@@ -234,7 +234,7 @@ function App() {
       </div>
 
       <main className="py-3 lg:pl-72">
-        <div className={classNames('absolute w-full flex justify-center mt-4', displayNotification ?  'block' : 'hidden' )}>
+        <div className={classNames('absolute w-full mt-4', displayNotification ?  'block' : 'hidden' )}>
           {notification !== null && <Alert value={notification}/>}
         </div>
         <Country
